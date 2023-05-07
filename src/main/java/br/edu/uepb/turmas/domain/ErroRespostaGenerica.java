b14bcb52076d3fe3ea885e45b1704b5248ee286e
Camila de Sousa <43892685+Milasousa@users.noreply.github.com>
2023-05-07 01:53:26 -0300

    mapeamento entre Entities e DTOs com Model Mapper

---
+++ src/main/java/br/edu/uepb/turmas/domain/ErroRespostaGenerica.java
@@ -0,0 +1,10 @@
+package br.edu.uepb.turmas.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErroRespostaGenerica {
+    private String error;
+}
---
+++ src/main/java/br/edu/uepb/turmas/dto/AlunoDTO.java
@@ -0,0 +1,12 @@
+package br.edu.uepb.turmas.dto;
+import lombok.Data;
+
+@Data
+public class AlunoDTO {
+    private Long IdMatricula;
+    private String nome;
+    private String email;
+}
+
+
+
